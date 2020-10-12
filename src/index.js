7a89cdf76444d88fa9e6a8d6bf49d9cc7b2a4e5a
Nurbek <musin.nurbek@inbox.ru>
2020-10-12 15:06:49 +0600

    feat: implemented the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let ar = n;
+    if(ar < 0 ){
+        ar = ar * (-1);
+    }
+    ar = String(ar);
+  let a = ar.split('');
+  let res = '';
+  for( let i = a.length-1; i >= 0 ; i--){
+    res = `${res}${a[i]}`;
+  }
+  res = Number(res);
+
+  return res;
 }
